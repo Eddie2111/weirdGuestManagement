af4e6dd6ddd564dd4a58d03d9e33e74c02f4d10d
ASM Tareq Mahmood <59513101+Eddie2111@users.noreply.github.com>
2022-04-12 18:23:05 +0600

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,245 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php
+  include 'proceeds/head.php';
+?>
+
+  <body>
+<?php
+  include 'proceeds/nav.php';
+?>
+
+
+    <!-- Begin Main Content -->
+    <div class="main-content">
+      <!-- Begin About Me Section -->
+
+      <!-- End About Me Content -->
+      <div class="section-dark ">
+        <center>
+        <div class="container">
+            <div class="row">
+              <div class="col">
+              <div class="card">
+                  <div class="card-content">
+                    <div class="content">
+                      Lorem ipsum leo risus, porta ac consectetur ac, vestibulum at eros. Donec id elit non mi porta gravida at eget metus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras mattis consectetur purus sit amet fermentum.
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col">
+              <div class="card">
+                    <div class="card-content">
+                      <div class="content">
+                        Lorem ipsum leo risus, porta ac consectetur ac, vestibulum at eros. Donec id elit non mi porta gravida at eget metus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras mattis consectetur purus sit amet fermentum.
+                      </div>
+                    </div>
+                  </div>
+              </div>
+              <div class="col">
+              <div class="card">
+                    <div class="card-content">
+                      <div class="content">
+                        Lorem ipsum leo risus, porta ac consectetur ac, vestibulum at eros. Donec id elit non mi porta gravida at eget metus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras mattis consectetur purus sit amet fermentum.
+                      </div>
+                    </div>
+                  </div>
+              </div>
+              <div class="col">
+              <div class="card">
+                    <div class="card-content">
+                      <div class="content">
+                        Lorem ipsum leo risus, porta ac consectetur ac, vestibulum at eros. Donec id elit non mi porta gravida at eget metus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras mattis consectetur purus sit amet fermentum.
+                      </div>
+                    </div>
+                  </div>
+              </div>
+            </div>
+        </div>
+      </center>
+      </div>
+      <!-- Begin Services Content -->
+      <div class="section-color " id="services">
+        <div class="container">
+          <!-- Begin Contact Content -->
+          <div class="section-light contact" id="contact">
+            <div class="container">
+              <div
+                class="columns is-multiline"
+                data-aos="fade-in-up"
+                data-aos-easing="linear"
+              >
+                <div class="column is-12 about-me">
+                  <h1 class="title has-text-centered section-title">
+                    Get in touch
+                  </h1>
+                </div>
+                <div class="column is-8 is-offset-2">
+                  <form
+                    action="proceeds/contactedpost.php"
+                    method="POST"
+                  >
+                    <div class="field">
+                      <label class="label">Name</label>
+                      <div class="control has-icons-left">
+                        <input
+                          class="input"
+                          type="text"
+                          placeholder="Ex. Jane Smith"
+                          name="name"
+                        />
+                        <span class="icon is-small is-left">
+                          <i class="fas fa-user"></i>
+                        </span>
+                      </div>
+                    </div>
+                    <div class="field">
+                      <label class="label">Email</label>
+                      <div class="control has-icons-left">
+                        <input
+                          class="input"
+                          type="email"
+                          placeholder="Ex. hello@arctheme.com"
+                          name="email"
+                        />
+                        <span class="icon is-small is-left">
+                          <i class="fas fa-envelope"></i>
+                        </span>
+                      </div>
+                    </div>
+                    <div class="field">
+                      <label class="label">Message</label>
+                      <div class="control">
+                        <textarea
+                          class="textarea"
+                          placeholder="Textarea"
+                          name="message"
+                        ></textarea>
+                      </div>
+                    </div>
+                    <div class="field">
+                      <div class="control ">
+                        <button class="button submit-button">
+                          Submit&nbsp;&nbsp;
+                          <i class="fas fa-paper-plane"></i>
+                        </button>
+                      </div>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+          <!-- End Contact Content -->
+        </div>
+      </div>
+      <!-- End Services Content -->
+      <!-- Begin Skills Content -->
+
+      <!-- End Skills Content -->
+      <!-- Begin Work Content -->
+      <div class="section-dark my-work" id="my-work" > <!-- our hotel -->
+        <div class="container">
+          <div
+            class="columns is-multiline"
+            data-aos="fade-in"
+            data-aos-easing="linear"
+          >
+            <div class="column is-12">
+              <h1 class="title has-text-centered section-title">Our Hotel</h1>
+            </div>
+            <div class="column is-3">
+              <a href="#">
+                <figure
+                  class="image is-2by1 work-item"
+                  style="background-image: url('https://picsum.photos/320/180?image=0');"
+                ></figure>
+              </a>
+            </div>
+            <div class="column is-3">
+              <a href="#">
+                <figure
+                  class="image is-2by1 work-item"
+                  style="background-image: url('https://picsum.photos/320/180?image=10');"
+                ></figure>
+              </a>
+            </div>
+            <div class="column is-3">
+              <a href="#">
+                <figure
+                  class="image is-2by1 work-item"
+                  style="background-image: url('https://picsum.photos/320/180?image=20');"
+                ></figure>
+              </a>
+            </div>
+            <div class="column is-3">
+              <a href="#">
+                <figure
+                  class="image is-2by1 work-item"
+                  style="background-image: url('https://picsum.photos/320/180?image=30');"
+                ></figure>
+              </a>
+            </div>
+            <div class="column is-3">
+              <a href="#">
+                <figure
+                  class="image is-2by1 work-item"
+                  style="background-image: url('https://picsum.photos/320/180?image=40');"
+                ></figure>
+              </a>
+            </div>
+            <div class="column is-3">
+              <a href="#">
+                <figure
+                  class="image is-2by1 work-item"
+                  style="background-image: url('https://picsum.photos/320/180?image=50');"
+                ></figure>
+              </a>
+            </div>
+            <div class="column is-3">
+              <a href="#">
+                <figure
+                  class="image is-2by1 work-item"
+                  style="background-image: url('https://picsum.photos/320/180?image=60');"
+                ></figure>
+              </a>
+            </div>
+            <div class="column is-3">
+              <a href="#">
+                <figure
+                  class="image is-2by1 work-item"
+                  style="background-image: url('https://picsum.photos/320/180?image=70');"
+                ></figure>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- End Work Content -->
+      <!-- Begin Contact Content -->
+
+      <!-- End Contact Content -->
+    </div>
+    <!-- End Main Content -->
+
+    <!-- Begin Footer -->
+<?php
+  include "footer.php";
+?>
+    <!-- End Footer -->
+
+    <!-- Scripts -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="js/showcase.js"></script>
+    <link href="https://cdn.rawgit.com/michalsnik/aos/2.1.1/dist/aos.css" rel="stylesheet">
+    <script src="https://cdn.rawgit.com/michalsnik/aos/2.1.1/dist/aos.js"></script>
+    <script>
+      AOS.init({
+        easing: "ease-out",
+        duration: 800,
+      });
+    </script>
+  </body>
+</html>
